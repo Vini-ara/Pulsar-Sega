c4ad83bb0e3b25951a66d3eeab5ce3145dc4dacf
Vinicius Araujo <vinicius.ar02@gmail.com>
2022-08-31 10:29:11 -0300

    fixed assets and started refactoring

--- main.s
+++ main.s
@@ -1,18 +1,20 @@
 .data 
+# game constants
 isrunnig: .byte 1
+
+# sprites names
 stage1: .string "stage1.bin"
 black_tile: .string "TankBlackTile.bin"
 tank: .string "tank.bin"
-tank_position: .half 70, 50		# (y, x) 
-tank_dimensions: .byte 12, 8	# (heingth, width)
-tank_direction: .byte 0 		# 0 = up, 1 = down, 2 = right, 3 = left
-tank_bin: .space 96
-tank_clear_bin: .space 96
 
-.macro SETUP
- 	OPEN_FILE(tank, tank_dimensions, tank_bin)
- 	OPEN_FILE(black_tile, tank_dimensions, tank_clear_bin)
-.end_macro
+# sprites data
+tank_bin: .space 64
+tank_clear_bin: .space 64
+
+# character info
+tank_position: .half 190, 20		# (y, x) 
+tank_dimensions: .byte 8, 8	# (heingth, width)
+tank_direction: .byte 0 		# 0 = up, 1 = down, 2 = right, 3 = left
 	
 .macro PRINT_MAP(%fileName)
  	la a0, %fileName 			# loads file-name string
@@ -31,30 +33,6 @@ tank_clear_bin: .space 96
  	mv a0, t0				# file descriptor
  	li a7, 57				# syscall to close file
  	ecall		
-.end_macro
-
-.macro OPEN_FILE(%fileName, %dimensions, %memoryDestination) 
-	la a0, %fileName
-	li a1, 0
-	li a2, 0
-	li a7, 1024
-	ecall
-	
-	mv t2, a0				# saves file descriptor returned from syscall
-	
-	la t0, %dimensions
-	lb t1, 0(t0)
-	lb t0, 1(t0)
-	mul t0, t0, t1
-	
-	mv a2, t0
-	la a1, %memoryDestination
-	li a7, 63
-	ecall
-	
-	mv a0, t0				# file descriptor
- 	li a7, 57				# syscall to close file
- 	ecall
 .end_macro
 
 # Takes a filename label and a starting position label(2 bytes)
@@ -100,8 +78,9 @@ up_line_loop:
 	
 	
 	addi t1, t1, 1			# adds 1 to the counter
-	addi t3, t3, 308		# goes to next line
-	ble t1, s2, up_loop		# print each line untill the image is over
+	addi t3, t3, 320		# goes to next line
+	sub t3, t3, s3
+	blt t1, s2, up_loop		# print each line untill the image is over
 	j print_end
 	
 print_down:  
@@ -120,7 +99,8 @@ down_line_loop:
 	addi t2, t2, 1
 	blt t2, s3, down_line_loop
 	
-	addi t3, t3, 308
+	addi t3, t3, 320		# goes to next line
+	sub t3, t3, s3
 	bgt t0, s1, down_loop
 	j print_end
 	
@@ -137,7 +117,7 @@ right_line_loop:
 	lb t4, 0(t0)
 	sb t4, 0(t3)
 	addi t3, t3, 1
-	addi t0, t0, -12
+	sub t0, t0, s2
 	addi t2, t2, 1
 	blt t2, s2, right_line_loop
 	
@@ -148,7 +128,8 @@ right_line_loop:
 	add t0, t1, t0
 	addi t5, t5, 1
 	add t0, t5, t0
-	addi t3, t3, 312
+	addi t3, t3, 320		# goes to next line
+	sub t3, t3, s3
 	blt t5, s3, right_loop
 	j print_end
 	
@@ -161,11 +142,12 @@ left_line_loop:
 	lb t4, 0(t0)
 	sb t4, 0(t3)
 	addi t3, t3, 1
-	addi t0, t0, 12
+	add t0, t0, s2
 	addi t2, t2, 1
 	blt t2, s2, left_line_loop
 	
-	addi t3, t3, 312
+	addi t3, t3, 320		# goes to next line
+	sub t3, t3, s3
 	mv t0, s1
 	addi t1, t1, 1
 	add t0, t0, t1
@@ -174,74 +156,111 @@ print_end:
 	
 .end_macro 
 
-.macro CHECK_KEYPRESS()
-	li t1,0xFF200000		# carrega o endere�o de controle do KDMMIO
-	lw t0,0(t1)				# Le bit de Controle Teclado
-	andi t0,t0,0x0001		# mascara o bit menos significativo
-   	beq t0,zero,loop 	   	# Se n�o h� tecla pressionada ent�o vai para FIM
-  	lw t2,4(t1)  			# le o valor da tecla tecla
-move_tank:
 
-	li t3, 119        # letra "w"
-	beq t2, t3, up    # se for "w" move para cima
-	li t3, 115        # letra "s" 
-	beq t2, t3, down  # se for "s" vai para baixo
-	li t3, 100        # letra "d"  
-	beq t2, t3, right # se for "d" vai para direita
-	li t3, 97         # letra "a" 
-	beq t2, t3, left  # se for letra "a" vai para esquerda
-	j loop            # se n for nada volta para o loop
-up:
+.text
+SETUP: 
+	la a0, tank
+	la t0, tank_dimensions
+	la s1, tank_bin
+	call OPEN_FILE
+	
+	la a0, black_tile
+	la t0, tank_dimensions
+	la s1, tank_clear_bin
+ 	call OPEN_FILE
+
+	
+	PRINT_MAP(stage1)	# prints stage 1 on screen
+	
+ GAME_LOOP:
+ 	call CHECK_KEYPRESS
+ 		
+  	PRINT_CHARACTER(tank_bin , tank_position, tank_dimensions, tank_direction)	# prints tank
+   	
+  	lb t0, isrunnig		# checks if game is still running
+  	bne t0, zero, GAME_LOOP
+  	
+ 
+# a0 = file name
+# t0 = dimensions
+# s1 = memory destination
+OPEN_FILE: 		
+	li a1, 0
+	li a2, 0
+	li a7, 1024
+	ecall
+	
+	mv t4, a0				# saves file descriptor returned from syscall
+	
+	lb t2, 0(t0)
+	lb t0, 1(t0)
+	mul t0, t0, t2
+	
+	mv a2, t0
+	mv a1, s1
+	li a7, 63
+	ecall
+	
+	mv a0, t4				# file descriptor
+ 	li a7, 57				# syscall to close file
+ 	ret
+ 	
+CHECK_KEYPRESS: 
+	li t1, 0xFF200000		# carrega o endere�o de controle do KDMMIO
+	lw t0, 0(t1)			# Le bit de Controle Teclado
+	andi t0, t0, 0x0001		# mascara o bit menos significativo
+   	beq t0, zero, FIM 	   	# Se n�o h� tecla pressionada ent�o vai para FIM
+  	lw t2, 4(t1)  			# le o valor da tecla tecla
+
+	li t3, 'w'
+	beq t2, t3, UP    # se for "w" move para cima
+	li t3, 's'
+	beq t2, t3, DOWN  # se for "s" vai para baixo
+	li t3, 'd'      
+	beq t2, t3, RIGHT # se for "d" vai para direita
+	li t3, 'a'         
+	beq t2, t3, LEFT  # se for letra "a" vai para esquerda
+	j FIM
+
+UP:
   	PRINT_CHARACTER(tank_clear_bin, tank_position, tank_dimensions, tank_direction) # clear ghost
 	la t0, tank_position 	# loads position addr
 	lh t1, 0(t0)		# loads y position value
-	addi t1, t1, -2		# moves up 2 pixels	
+	addi t1, t1, -4		# moves up 2 pixels	
 	sh t1, 0(t0)		# saves new value back in memory
 	la t0, tank_direction	# loads direction addr
 	li t2, 0		# digit 0 reference
 	sb t2, 0(t0)		# saves 0 as direction val
-	j loop
-down:
+	j FIM 
+DOWN:
   	PRINT_CHARACTER(tank_clear_bin, tank_position, tank_dimensions, tank_direction) # clear ghost
 	la t0, tank_position    # loads position addr
 	lh t1, 0(t0)		# loads y position value
-	addi t1, t1, 2		# moves down 2 pixels
+	addi t1, t1, 4		# moves down 2 pixels
 	sh t1, 0(t0)		# saves new value back in memory
 	la t0, tank_direction	# loads direction addr
 	li t2, 1		# digit 1 reference
 	sb t2, 0(t0)		# saves 1 as direction val
-	j loop
-left: 
-  	PRINT_CHARACTER(tank_clear_bin, tank_position, tank_dimensions, tank_direction)	# clear ghost
-	la t0, tank_position	# loads position addr
-	lh t1, 2(t0)		# loads x postion value
-	addi t1, t1, -2		# moves	left 2 pixels
-	sh t1, 2(t0)		# saves new value back in memory
-	la t0, tank_direction	# loads direction addr
-	li t2, 3		# digit 3 reference
-	sb t2, 0(t0)		# saves 3 as direction val
-	j loop
-right:
+	j FIM 
+RIGHT:
   	PRINT_CHARACTER(tank_clear_bin, tank_position, tank_dimensions, tank_direction) # clear ghost
 	la t0, tank_position	# loads position addr
 	lh t1, 2(t0)		# loads x position value
-	addi t1, t1, 2		# moves right 2 pixels
+	addi t1, t1, 4		# moves right 2 pixels
 	sh t1, 2(t0)		# saves new value back in memory	
 	la t0, tank_direction	# loads direction addr
 	li t2, 2		# digit 2 reference
 	sb t2, 0(t0)		# saves 2 as direction val
-	j loop
-.end_macro
+	j FIM
+LEFT: 
+  	PRINT_CHARACTER(tank_clear_bin, tank_position, tank_dimensions, tank_direction)	# clear ghost
+	la t0, tank_position	# loads position addr
+	lh t1, 2(t0)		# loads x postion value
+	addi t1, t1, -4		# moves	left 2 pixels
+	sh t1, 2(t0)		# saves new value back in memory
+	la t0, tank_direction	# loads direction addr
+	li t2, 3		# digit 3 reference
+	sb t2, 0(t0)		# saves 3 as direction val
+	j FIM 
+FIM: 	ret
 
-.text
-	SETUP			# initial game setup
-	PRINT_MAP(stage1)	# prints stage 1 on screen
- loop:	
-  	PRINT_CHARACTER(tank_bin , tank_position, tank_dimensions, tank_direction)	# prints tank
-   	CHECK_KEYPRESS()	# checks if any key is pressed
-  	lb t0, isrunnig		# checks if game is still running
-  	bne t0, zero, loop
-  		
-FIM: 
- 	li a7, 10
- 	ecall
